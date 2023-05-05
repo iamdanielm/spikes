58bea195d3860e5320bd6cc7dc9b869122aeb2ce
Daniel <daniel.maydana@cision.com>
2023-05-05 01:08:46 -0400

    Add more comments

--- UFCO-507-test-ui5-table/README.md
+++ UFCO-507-test-ui5-table/README.md
@@ -1,6 +1,6 @@
 # UFCO-507: Test ui5 table
 
-## Make it run
+## Run locally
 
 To run it please:
 - run `npm i`.
@@ -9,7 +9,7 @@ To run it please:
 
 ## Cheat sheet
 
-These is a compact summary of the official [ui5 table documentation](https://sap.github.io/ui5-webcomponents/playground/components/Table/)
+These is a compact summary of the official [ui5 table documentation](https://sap.github.io/ui5-webcomponents/playground/components/Table/).
 #### Table column
 - Attributes: demandPopin, minWidth, popinDisplay, popinText.
 #### Table row
@@ -21,8 +21,12 @@ These is a compact summary of the official [ui5 table documentation](https://sap
 
 ## Notes
 
-- Official documentation recomends to include only text in the cells. Not realistic.
-- I haven't found a way to select a row group. It's either the whole table or nothing. Maybe it can be done through the ui5 API.
-- The row-group element feels more like a sub-header than a element to group rows.
+- I haven't found a way to select the rows inside a `ui-5-table-row-group` element. It's either the whole table or nothing. Maybe it can be done through the ui5 API.
+- The `ui-5-table-row-group` element feels more like a sub-header than a element to group rows.
+- Design doesn't include row groups in their examples.
 - I was able to style ui5 elements selecting the slots 🤦‍♂️.
 - I'm missing something to be able to override ui5/sap css variables.
+- ui5-prefix variables use sap-prefix variables.
+- Official documentation recomends to include only text in the cells. Not realistic.
+- There seems to be no issues with slotting any element inside a `ui5-table-cell` element.
+- 
--- UFCO-507-test-ui5-table/index.js
+++ UFCO-507-test-ui5-table/index.js
@@ -6,30 +6,35 @@ const table = document.createElement('ui5-table')
 table.setAttribute('mode', 'MultiSelect')
 content.appendChild(table)
 
+
+
 const createColumnHeaders = (data, parent) => {
     Object.keys(data[0]).forEach((current, i) => {
         const column = document.createElement('ui5-table-column')
         const label = document.createElement('ui5-label')
 
-        if(current !== 'group') {
+        if (current !== 'group') {
             label.innerHTML = camelCaseToNormalCase(current)
-            column.setAttribute('id', `column-${i + 1}`)
-            column.setAttribute('slot', 'columns')
-            column.appendChild(label)
-            parent.appendChild(column)
         }
+        else {
+            label.innerHTML = ''
+        }
+        column.setAttribute('id', `column-${i + 1}`)
+        column.setAttribute('slot', 'columns')
+        column.appendChild(label)
+        parent.appendChild(column)
     })
 }
 
 const populateTable = (data, parent) => {
-    const groupIds = [1,2,3,4,5]
+    const groupIds = [1, 2, 3, 4, 5]
 
     groupIds.forEach((groupId) => {
-        
+
         createRowGroup(`Group ${groupId}`, parent)
 
         data.forEach(entry => {
-            if(entry.group === groupId) {
+            if (entry.group === groupId) {
                 createRow(entry, parent)
             }
         })
@@ -51,20 +56,43 @@ const createRow = (data, parent) => {
     Object.entries(data).forEach(entry => {
         const [key, value] = entry
 
-        if(key !== 'group') {
-            createCell(value, row)
+        if (key !== 'group') {
+            createTextCell(value, row)
+        }
+        else {
+            const editRowContainer = document.createElement('div')
+            const editButton = document.createElement('atlas-button')
+            const closeButton = document.createElement('atlas-button')
+            const editIcon = document.createElement('atlas-icon')
+            const closeIcon = document.createElement('atlas-icon')
+            editRowContainer.classList.add('edit-row-controls')
+            editButton.appendChild(editIcon)
+            closeButton.appendChild(closeIcon)
+            editButton.setAttribute('kind', 'icon')
+            closeButton.setAttribute('kind', 'icon')
+            editIcon.setAttribute('name', 'edit')
+            closeIcon.setAttribute('name', 'close')
+            console.log(editButton, closeButton)
+            editRowContainer.appendChild(editButton)
+            editRowContainer.appendChild(closeButton)
+            createElementCell([editRowContainer], row)
         }
     })
 }
 
-const createCell = (data, parent) => {
+const createElementCell = (elements, parent) => {
+    const cell = document.createElement('ui5-table-cell')
+    elements.forEach(element => cell.appendChild(element))
+    parent.appendChild(cell)
+}
+
+const createTextCell = (data, parent) => {
     const cell = document.createElement('ui5-table-cell')
     const span = document.createElement('span')
     span.innerText = `${data}`
     cell.append(span)
     parent.appendChild(cell)
 }
-
 
 createColumnHeaders(data, table)
 populateTable(data, table)
